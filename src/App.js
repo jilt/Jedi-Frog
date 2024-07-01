dce09af5afab73ecc740a771b64b936081ad1fc4
Laura Camellini <jilt@jeeltcraft.com>
2024-07-01 02:20:47 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -15,95 +15,18 @@ function App() {
     const [newStatus, setNewStatus] = useState('');
     const [account, setAccount] = useState('');
     const [contract, setContract] = useState({});
-    const [fam, setFam] = useState(false);
-
+    const { REACT_APP_DUNE } = process.env;
 
     let selectedAccount;
-
     let provider;
 
-    // Fam ABI import
-
-    const erc20ABI = [
-        {
-            constant: true,
-
-            inputs: [{ name: '_owner', type: 'address' }],
-
-            name: 'balanceOf',
-
-            outputs: [{ name: 'balance', type: 'uint256' }],
-
-            type: 'function',
-        },
-    ];
-
-    // Fam tokens addresses
-
-    const efrogs = "0x194395587d7b169e63eaf251e86b1892fa8f1960";
-    const croack = "0xacb54d07ca167934f57f829bee2cc665e1a5ebef";
-    const lxp = "0xd83af4fbd77f3ab65c3b1dc4b38d7e67aecf599a";
-
-    // check Fam ownership
-
-    const lineaProv = process.env.INFURA_RPC;
-
-    const LineaClient = new Web3(new Web3.providers.HttpProvider(lineaProv));
-
-    var web3linea = new Web3(LineaClient);
-
-    const croackcontract = new web3linea.eth.Contract(erc20ABI, croack);
-    const lxpcontract = new web3linea.eth.Contract(erc20ABI, lxp);
-    const efrogcontract = new web3linea.eth.Contract(erc20ABI, efrogs);
-
-    const getDev = async (add) => {
-
-        if (add = "0xEA867Afff7Ca46441424F499D4c30BCc34d8b6E5") {
-            setFam(true);
-            console.log('is dev');
-        }
-    };
-
-    const getFam2 = async (add) => {
-
-        const isFam = await lxpcontract.methods.balanceOf(add).call();
-        if (isFam > 0) {
-            setFam(true);
-        } else {
-            getDev(add);
-            console.log("not lxp");
-        }
-    };
-
-    const getFam1 = async (add) => {
-
-        const isFam = await efrogcontract.methods.balanceOf(add).call();
-        if (isFam > 0) {
-            setFam(true);
-        } else {
-            getFam2(add)
-            console.log("not efrog");
-        }
-
-    };
-
-    const getFam = async (add) => {
-
-        const isFam = await croackcontract.methods.balanceOf(add).call();
-        if (isFam > 0) {
-            setFam(true);
-        } else {
-            getFam1(add);
-            console.log("not croack");
-        }
-
-    };
+    
 
     const init = async () => {
 
         // get addresses of EigerLAyer investors
 
-        // const dune = new Dune('process.env.DUNE_API');
+        // const dune = new Dune('REACT_APP_DUNE');
         // const execute = await dune.execute(3592801);
         // const eiger = await dune.results(execute.data.execution_id);
 
@@ -223,7 +146,6 @@ function App() {
         let isValid = await isValidAddress(account);
         if (isValid) {
             try {
-                getFam(account);
                 let timeline = await contract.methods.getStatus(account).call();
                 setFeed(timeline);
                 let previous = await contract.methods.statuses(account).call();
@@ -244,7 +166,7 @@ function App() {
             <header className="bg-gray-dark sticky top-0 z-50">
                 <div className="container mx-auto flex justify-between items-center py-4">
                 <div className="flex items-center">
-                    <img src="assets/images/frog-logo.png" alt="Logo" className="h-14 w-auto mr-4" />
+                        <img src={ logo } alt="Logo" className="h-14 w-auto mr-4" />
                 </div>
 
                 <div className="flex md:hidden">
@@ -371,7 +293,7 @@ function App() {
                     </div>
 
                     <div className="flex flex-col items-center md:items-start">
-                        <img src="assets/images/frog-logo.png" alt="Logo" className="h-14 w-auto mb-4"/>
+                        <img src={ logo } alt="Logo" className="h-14 w-auto mb-4"/>
                             <p>Developed by <a href="https://spacema-dev.com/" className="text-primary hover:text-secondary font-bold">Spacema-dev</a> and <a href="https://www.jeeltcraft.com/" className="text-primary hover:text-secondary font-bold">Jeeltcraft</a></p>
                     </div>
                 </div>
--- src/EventCard.js
+++ src/EventCard.js
@@ -2,12 +2,12 @@ import Web3 from "web3";
 import ABI from './contracts/StatusContract.json';
 import React, { useEffect, useState } from "react";
 import EventDate from './Date';
+const { REACT_APP_INFURA_RPC } = process.env;
 
-
-const EventCard = ({ feed }) => {
+const EventCard = ({ feed, address }) => {
     const [events, setEvents] = useState([]);
+    const [fam, setFam] = useState(false);
     let provider = window.ethereum;
-    let selectedAccount;
     const eventName = "StatusUpdated";
     const web3 = new Web3(provider);
 
@@ -15,31 +15,113 @@ const EventCard = ({ feed }) => {
 
     const contract = new web3.eth.Contract(ABI.abi, contractAddress);
 
-    provider.request({ method: 'eth_requestAccounts' })
-        .then(accounts => {
-            selectedAccount = accounts[0];
-            // console.log(`Selected account for events: ${selectedAccount}`);
-        })
-        .catch(error => { console.log(error); });
+    // gating on Fam contracts
 
+    // Fam ABI import
+
+    const erc20ABI = [
+        {
+            constant: true,
+
+            inputs: [{ name: '_owner', type: 'address' }],
+
+            name: 'balanceOf',
+
+            outputs: [{ name: 'balance', type: 'uint256' }],
+
+            type: 'function',
+        },
+    ];
+
+    // Fam tokens addresses
+
+    const efrogs = "0x194395587d7b169e63eaf251e86b1892fa8f1960";
+    const croack = "0xacb54d07ca167934f57f829bee2cc665e1a5ebef";
+    const lxp = "0xd83af4fbd77f3ab65c3b1dc4b38d7e67aecf599a";
+
+    // check Fam ownership
+
+    const lineaProv = REACT_APP_INFURA_RPC;
+    console.log(lineaProv);
+
+    const LineaClient = new Web3(new Web3.providers.HttpProvider(lineaProv));
+
+    var web3linea = new Web3(LineaClient);
+
+    const croackcontract = new web3linea.eth.Contract(erc20ABI, croack);
+    const lxpcontract = new web3linea.eth.Contract(erc20ABI, lxp);
+    const efrogcontract = new web3linea.eth.Contract(erc20ABI, efrogs);
+
+    const getDev = async (add) => {
+        if (add === "0xea867afff7ca46441424f499d4c30bcc34d8b6e5") {
+            setFam(true);
+        }
+    };
+
+    const getFam2 = async (add) => {
+
+        const isFam = await lxpcontract.methods.balanceOf(add).call();
+        if (isFam > 0) {
+            setFam(true);
+        } else {
+            getDev(add);
+           // console.log("not lxp");
+        }
+    };
+
+    const getFam1 = async (add) => {
+
+        const isFam = await efrogcontract.methods.balanceOf(add).call();
+        if (isFam > 0) {
+            setFam(true);
+        } else {
+            getFam2(add)
+            // console.log("not efrog");
+        }
+
+    };
+
+    const getFam = async (add) => {
+        let isValid = await isValidAddress(add);
+        if (isValid) {
+            const isFam = await croackcontract.methods.balanceOf(add).call();
+            if (isFam > 0) {
+                setFam(true);
+            } else {
+                getFam1(add);
+                // console.log("not croack");
+            }
+        }
+    };
+
+    const isValidAddress = (adr) => {
+        try {
+            const web3 = new Web3();
+            web3.utils.toChecksumAddress(adr);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
 
     useEffect(() => {
-        const getEvents = async (contract) => {
+        const getEvents = async (contract, address) => {
             try {
                 let events = await contract.getPastEvents(eventName, {
-                    filter: {},
+                    filter: {
+                        user: address,
+                    },
                     fromBlock: 1747337,
                     toBlock: "latest"
                 });
                 let display = events.reverse();
                 setEvents(display);
-                // console.log(display);
             } catch (error) {
                 console.log(error);
             }
-            // setList(events);
         }
-        getEvents(contract, selectedAccount);
+        getEvents(contract, address);
+        getFam(address);
     }, []);
 
     return (
@@ -48,8 +130,8 @@ const EventCard = ({ feed }) => {
                 {events.map(function (data, index) {
                     return (
                         <li className="flex items-center mb-4" key={index}>
-                            <strong className="bg-primary text-white rounded-full w-8 h-8 text-lg font-semibold flex items-center justify-center mr-3">{index}</strong>
-                            <span className="text-white">{data.returnValues[1]}</span> <br /><EventDate timestamp={data.blockNumber}/>
+                            <strong className="bg-primary text-white rounded-full w-8 h-8 text-lg font-semibold flex items-center justify-center mr-3">{((events.length) - index)}</strong>
+                            <span className="text-white">{data.returnValues[1]}</span> {!fam ? <EventDate timestamp={data.blockNumber} /> : <span class="text-right text-primary date">Jedi question</span>}
                         </li>
                     )
                 })}
